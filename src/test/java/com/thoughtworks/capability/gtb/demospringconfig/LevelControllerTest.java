dd4f8f3b1f24fcf9b5a911e9b36feb450df555b3
twodogbeta <qijinhaoup@163.com>
2020-09-20 23:39:11 +0800

    [qjh] solve p2 method2

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {"levelNumber = 2"})
+class LevelControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnRightValueWhenCallInterface() throws Exception {
+        mockMvc.perform(get("/level")).andExpect(content().string("advanced"));
+    }
+}
